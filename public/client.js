1de57d3b494508bc2da670fc200b955b1658d14e
Parallaxsid <mixasicsid@mail.ru>
2020-08-09 14:30:52 +0300

    Second commit

--- public/client.js
+++ public/client.js
@@ -34,3 +34,106 @@ socket.on("userMessage", (data) => {
 socket.on('userTyping', (data) => {
     typing.innerHTML = '<p><em>' + data + ' is typing... </em></p>'
 })
+
+
+function getLVideo(callbacks){
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    var contraints = {
+        audio:true,
+        video:true
+    }
+    navigator.getUserMedia(contraints, callbacks.success, callbacks.error)
+
+}
+
+function recStream(stream, elemid){
+    var video = document.getElementById(elemid);
+
+    video.srcObject = stream;
+
+    window.peer_stream = stream;
+}
+
+getLVideo({
+    success: function(stream){
+        window.localstream = stream;
+        recStream(stream, 'lVideo')
+    },
+    error: function(err){
+        alert("cannot acess your camera");
+        console.log(err);
+    }
+})
+
+
+var conn;
+var peer_id;
+
+var peer = new Peer();
+
+peer.on('open', function(){
+    document.getElementById("displayId").innerHTML = peer.id
+})
+
+
+peer.on('connection', function(connection){
+    conn = connection;
+    peer_id = connection.peer;
+
+    document.getElementById('connId').value = peer.id;
+});
+
+peer.on('error', function(err){
+    alert("an error has happened:" + err);
+    console.log(err);
+})
+
+
+document.getElementById('conn_button').addEventListener('click', function(){
+    peer_id = document.getElementById("connId").value;
+
+    if(peer_id){
+        conn = peer.connect(peer_id)
+
+    } else {
+        alert ("enter an id");
+        return false;
+
+    }
+})
+
+
+peer.on('call', function(call){
+
+    var acceptCall = confirm("Do you want to answer this call?");
+
+    if(acceptCall){
+        call.answer(window.localstream);
+
+        call.on('stream', function(stream){
+            window.peer_stream = stream;
+
+            recStream(stream, 'rVideo')
+        });
+
+        call.on('close', function(){
+            alert('The call has behind')
+        })
+    } else {
+        console.log("call denied")
+    }
+});
+
+
+document.getElementById('call_button').addEventListener('click', function(){
+    console.log("calling a peer:" + peer_id);
+    console.log(peer);
+
+    var call = peer.call(peer_id, window.localstream);
+
+    call.on('stream', function(stream){
+        window.peer_stream = stream;
+
+        recStream(stream, 'rVideo');
+    });
+})
